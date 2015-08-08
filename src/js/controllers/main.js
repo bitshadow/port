bb8c4f965d8cfc904fbdf07d6911b6630ca663be
Jignesh Kakadiya <jignesh.kakadiya@inmobi.com>
2015-08-09 00:06:36 +0530

    listen to hashchange

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -4,11 +4,13 @@ angular.module('App.controllers', [])
     .controller('MainController', ['$scope', '$location', function($scope, $location) {
         var tabs = ['about', 'projects'];
 
-        var selectedIndex = tabs.forEach(function(tab, i) {
-            if($location.url() === '/' + tab) {
-                $scope.selectedIndex = i;
-            }
-        });
+        $scope.$on("$stateChangeSuccess", function () {
+            var selectedIndex = tabs.forEach(function(tab, i) {
+                if($location.url() === '/' + tab) {
+                    $scope.selectedIndex = i;
+                }
+            });
+        })
 
         $scope.main.tabs = tabs;
     }])
