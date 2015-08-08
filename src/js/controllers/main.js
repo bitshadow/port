825d4a2c6165c7cc9fdf608178e48a3d7f364a1f
Jignesh Kakadiya <jignesh.kakadiya@inmobi.com>
2015-08-08 22:20:12 +0530

    default route fix

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,7 +1,14 @@
 'use strict';
 
 angular.module('App.controllers', [])
-    .controller('MainController', ['$scope', function($scope) {
+    .controller('MainController', ['$scope', '$location', function($scope, $location) {
         var tabs = ['about', 'projects'];
+
+        var selectedIndex = tabs.forEach(function(tab, i) {
+            if($location.url() === '/' + tab) {
+                $scope.selectedIndex = i;
+            }
+        });
+
         $scope.main.tabs = tabs;
     }])
